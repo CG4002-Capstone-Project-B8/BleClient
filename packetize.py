c57c8ceeffe248690fca616623c01a59b1e601d2
theeugenechong <eugenechong20011@gmail.com>
2022-09-09 17:23:33 +0800

    Add packet methods for new packet format

--- packetize.py
+++ packetize.py
@@ -2,7 +2,17 @@ import struct
 
 from constants import TPacketType
 
-CHECKSUM_POS = 21
+CHECKSUM_POS = 19
+
+PACKET_TYPE_SHIFT = 6
+SEQNUM_SHIFT = 5
+PLAYER_ID_SHIFT = 4
+DEVICE_ID_SHIFT = 2
+
+PACKET_TYPE_MASK = (3 << PACKET_TYPE_SHIFT)
+SEQNUM_MASK = (1 << SEQNUM_SHIFT)
+PLAYER_ID_MASK = (1 << PLAYER_ID_SHIFT)
+DEVICE_ID_MASK = (3 << DEVICE_ID_SHIFT)
 
 
 def get_checksum(byte_seq):
@@ -12,10 +22,16 @@ def get_checksum(byte_seq):
     return checksum
 
 
-# packet_type will be passed in as TPacketType.value, ack_seqnum is 0 or 1 and player_device_id will be b'\x01'
-def serialize(packet_type, ack_seqnum, player_device_id):
-    fmt = 'BBc18sc2s'
-    packet = struct.pack(fmt, packet_type, ack_seqnum, player_device_id, bytes(18), bytes(1), bytes(2))
+def createPacket(packet_type, ack_seqnum, player_id, device_id):
+    details = detailsAsBytes(packet_type.value, ack_seqnum, player_id, device_id)
+    return serialize(details)
+
+
+# details will be passed in as a bytes object
+def serialize(details):
+    # fmt = 'BBc18sc2s' - this is the format for the old packet
+    fmt = 'c18sc'
+    packet = struct.pack(fmt, details, bytes(18), bytes(1))
 
     packet_arr = bytearray(packet)
     packet_arr[CHECKSUM_POS] = get_checksum(packet[:CHECKSUM_POS])
@@ -25,8 +41,35 @@ def serialize(packet_type, ack_seqnum, player_device_id):
 
 
 def deserialize(packet):
-    fmt = '<BBc3h3fc2s'
-    return struct.unpack(fmt, packet)
+    fmt = '<c3h3fc'
+    packet_attr = struct.unpack(fmt, packet)
+
+    # packet_attr returns a tuple, details is the first element ([0]) of the tuple (a bytes object)
+    # to get the first (and only) byte of the bytes object, another [0] is added
+    details = packet_attr[0][0]
+    details = interpretDetails(details)
+
+    # remove first element of the tuple
+    packet_attr = packet_attr[1:]
+
+    return details + packet_attr
+
+
+def detailsAsBytes(packet_type, ack_seqnum, player_id, device_id):
+    details = bytes(1)
+    details |= packet_type << PACKET_TYPE_SHIFT
+    details |= ack_seqnum << SEQNUM_SHIFT
+    details |= player_id << PLAYER_ID_SHIFT
+    details |= device_id << DEVICE_ID_SHIFT
+    return details
+
+
+def interpretDetails(details):
+    packet_type = details >> PACKET_TYPE_SHIFT
+    seqnum = (details & SEQNUM_MASK) >> SEQNUM_SHIFT
+    player_id = (details & PLAYER_ID_MASK) >> PLAYER_ID_SHIFT
+    device_id = (details & DEVICE_ID_MASK) >> DEVICE_ID_SHIFT
+    return packet_type, seqnum, player_id, device_id
 
 
 def isInvalidPacket(data):
