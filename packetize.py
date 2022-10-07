19bab7395d9861649bad380f4e2a28207cf085a8
theeugenechong <eugenechong20011@gmail.com>
2022-10-07 08:31:41 +0800

    Add code for detecting shot and receiving shot

--- packetize.py
+++ packetize.py
@@ -79,7 +79,12 @@ def interpretDetails(details):
 
     # new bits introduced for status of player
     sent_shot = (details & SEND_SHOT_MASK) >> SEND_SHOT_SHIFT
+    if sent_shot:
+        print("SENT SHOT!\n\n")
+
     received_shot = (details & RECEIVE_SHOT_MASK) >> RECEIVE_SHOT_SHIFT
+    if received_shot:
+        print("RECEIVED SHOT!\n\n")
 
     return packet_type, seqnum, player_id, device_id, sent_shot, received_shot
 
