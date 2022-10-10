39ea178927766a86d9f56a10dbec084af5c3b1ff
theeugenechong <eugenechong20011@gmail.com>
2022-10-10 10:52:28 +0800

    Add file for global variables

---
+++ globals.py
@@ -0,0 +1,9 @@
+from multiprocessing import Value
+
+IMU = 1
+EMITTER = 2
+RECEIVER = 3
+device_dict = {IMU: 'IMU', EMITTER: 'Emitter', RECEIVER: 'Receiver'}
+
+num_currently_connected_beetles = Value('i', 0)
+TOTAL_BEETLES = 3
